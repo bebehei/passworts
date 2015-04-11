6f4c671615d6c7b7bcfc232f08794ed2480af323
Stunkymonkey <felix.christian.buehler@web.de>
2015-04-11 14:16:05 +0200

    cleaning up

--- online/config.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-CSRF_ENABLED = True
-SECRET_KEY = str(''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)]))
-APPNAME = "Passworts"
-DEBUG=True
